60a4ff8c0f042751dfd03a2116e906d33470f191
vera-codes6 <vera.codes6@gmail.com>
2025-12-11 11:37:34 -0500

    Starting from scratch

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,47 @@
-pub mod backtracking;
-pub mod big_integer;
-pub mod bit_manipulation;
-pub mod ciphers;
-pub mod compression;
-pub mod conversions;
-pub mod data_structures;
-pub mod dynamic_programming;
-pub mod financial;
-pub mod general;
-pub mod geometry;
-pub mod graph;
-pub mod greedy;
-pub mod machine_learning;
-pub mod math;
-pub mod navigation;
-pub mod number_theory;
-pub mod searching;
-pub mod sorting;
-pub mod string;
+// pub mod backtracking;
+// pub mod big_integer;
+// pub mod bit_manipulation;
+// pub mod ciphers;
+// pub mod compression;
+// pub mod conversions;
+// pub mod data_structures;
+// pub mod dynamic_programming;
+// pub mod financial;
+// pub mod general;
+// pub mod geometry;
+// pub mod graph;
+// pub mod greedy;
+// pub mod machine_learning;
+// pub mod math;
+// pub mod navigation;
+// pub mod number_theory;
+// pub mod searching;
+// pub mod sorting;
+// pub mod string;
 
-#[cfg(test)]
-mod tests {
-    use std::vec;
+// #[cfg(test)]
+// mod tests {
+//     use std::vec;
 
-    use super::sorting;
-    #[test]
-    fn quick_sort() {
-        //descending
-        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
-        sorting::quick_sort(&mut ve1);
+//     use super::sorting;
+//     #[test]
+//     fn quick_sort() {
+//         //descending
+//         let mut ve1 = vec![6, 5, 4, 3, 2, 1];
+//         sorting::quick_sort(&mut ve1);
 
-        assert!(sorting::is_sorted(&ve1));
+//         assert!(sorting::is_sorted(&ve1));
 
-        //randomized
-        let mut ve2 = vec![3, 6, 1, 5, 2, 4];
-        sorting::quick_sort(&mut ve2);
+//         //randomized
+//         let mut ve2 = vec![3, 6, 1, 5, 2, 4];
+//         sorting::quick_sort(&mut ve2);
 
-        assert!(sorting::is_sorted(&ve2));
+//         assert!(sorting::is_sorted(&ve2));
 
-        //pre-sorted
-        let mut ve3 = vec![1, 2, 3, 4, 5, 6];
-        sorting::quick_sort(&mut ve3);
+//         //pre-sorted
+//         let mut ve3 = vec![1, 2, 3, 4, 5, 6];
+//         sorting::quick_sort(&mut ve3);
 
-        assert!(sorting::is_sorted(&ve3));
-    }
-}
+//         assert!(sorting::is_sorted(&ve3));
+//     }
+// }
